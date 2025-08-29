e5e28a399076e2df6b08a90c3d5738cf0e213d1a
Hein Htet Zaw <maintain1346@gmail.com>
2025-08-29 20:11:24 +0630

    fixed test fail:

--- tests/test_picture.py
+++ tests/test_picture.py
@@ -1,7 +1,7 @@
 import pytest
 
 @pytest.mark.order(6)
-def test_health(client):
+def test_picture(client):
     client = client[0]
     response = client.get("/picture")
     assert response.status_code == 200
