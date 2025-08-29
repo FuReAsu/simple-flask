f9a516201f0a0c7a3c26d718a0e0357900c996c4
Hein Htet Zaw <maintain1346@gmail.com>
2025-08-29 20:10:59 +0630

    fixed route error

--- app/simpleflask/picture.py
+++ app/simpleflask/picture.py
@@ -2,6 +2,6 @@ from flask import Blueprint, render_template
 
 picture_bp = Blueprint('picture', __name__)
 
-@picture_bp.route("/", methods=["GET"])
+@picture_bp.route("/picture", methods=["GET"])
 def picture():
     return render_template('picture.html')
