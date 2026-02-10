8efc9ba109b806009f8f4c443fa84263e3ef3519
Hein Htet Zaw <maintain1346@gmail.com>
2026-02-10 20:13:33 +0630

    removed pytests

--- tests/test_health.py
+++
@@ -1,8 +0,0 @@
-import pytest
-
-@pytest.mark.order(1)
-def test_health(client):
-    client = client[0]
-    response = client.get("/health")
-    assert response.status_code == 200
-    assert response.get_json() == {"Status": "Healthy"}
--- tests/test_picture.py
+++
@@ -1,7 +0,0 @@
-import pytest
-
-@pytest.mark.order(6)
-def test_picture(client):
-    client = client[0]
-    response = client.get("/picture")
-    assert response.status_code == 200
